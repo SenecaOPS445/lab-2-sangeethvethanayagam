6534c88c5b62d5c60a12d6a4c7d07be4ebe92bae
sangeethvethanayagam <sangeeth.vethanayagam@gmail.com>
2025-01-22 00:30:22 -0500

    Update lab2d.py

--- lab2d.py
+++ lab2d.py
@@ -4,9 +4,9 @@ import sys
 
 if len(sys.argv) != 3:
     print('Usage: ' +  sys.argv[0] +  ' name ' + 'age')
-    sys.exit(1)
+    sys.exit()
 
 name = sys.argv[1]
 age = sys.argv[2]
 
-print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+print('Hi ' + name + ', you are ' + age + ' years old.')
