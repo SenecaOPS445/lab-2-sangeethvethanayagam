cc395f965f030591b43d9f711b9f8f87a205f1f8
sangeethvethanayagam <sangeeth.vethanayagam@gmail.com>
2025-01-21 23:51:45 -0500

    Create lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Sangeeth Vethanayagam
+# Author ID: 189353238
+# Date Created: 01/25/2025
+
+timer = 10
+
+count = 10
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
+
